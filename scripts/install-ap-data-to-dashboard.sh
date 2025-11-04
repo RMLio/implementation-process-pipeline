33b2db659cd659f56a1f755c3dee165500f2a2c0
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2025-11-04 11:57:00 +0100

    Update ap-data-in-dashboard usage

--- scripts/install-ap-data-to-dashboard.sh
+++ scripts/install-ap-data-to-dashboard.sh
@@ -3,7 +3,4 @@
 cd ap-data-to-dashboard
 npm i
 npm run setup
-rm -rf subprojects && mkdir subprojects && pushd subprojects
-git clone -b v2.1.1 https://github.com/SolidLabResearch/miravi-a-linked-data-viewer.git
-(cd miravi-a-linked-data-viewer/main && npm i)
-popd
+node src/shacl-to-template.js
