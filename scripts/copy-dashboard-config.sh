04db41b560ad2e465d9d470e6aacb7b381760d3c
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2025-11-14 11:41:58 +0100

    Fix creation of dirs

--- scripts/copy-dashboard-config.sh
+++ scripts/copy-dashboard-config.sh
@@ -4,6 +4,7 @@ DIRECTORY=dashboard-config
 
 if [ -d "$DIRECTORY" ]; then
   echo "$DIRECTORY exists."
+  rm -rf ap-data-to-dashboard/miravi-initial-config
   mv $DIRECTORY ap-data-to-dashboard/miravi-initial-config
 else
   echo "$DIRECTORY doesn't exist. Using initial config."
