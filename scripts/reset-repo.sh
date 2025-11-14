28db508a6cdff6a17ed02c868430e3829270544f
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2025-11-14 09:20:14 +0100

    Fix #5

--- scripts/reset-repo.sh
+++ scripts/reset-repo.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-./reset-readme.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+$SCRIPT_DIR/reset-readme.sh
 rm -rf example-data \
   mappings \
   in-shacl \
