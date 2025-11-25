e2f3fe81164ca04f7981d52e02f2f359a134923e
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2025-11-25 11:19:04 +0100

    Fix script permissions, use placeholder where needed

--- scripts/move-miravi-dist.sh
+++ scripts/move-miravi-dist.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
 CURRENT_BRANCH=$1
+USE_DIST=$2
 
 if [[ "$CURRENT_BRANCH" == "main" ]]; then
   echo "Not using subdirectory because branch is main."
-  mv ap-data-to-dashboard/node_modules/miravi/main/dist/* gh-pages
+
+  if [[ "$USE_DIST" == "true" ]]; then
+    mv ap-data-to-dashboard/node_modules/miravi/main/dist/* gh-pages
+  else
+    cp scripts/dashboard-placeholder.html gh-pages/index.html
+  fi
 else
   echo "Using subdirectory because branch is not main."
   mkdir -p gh-pages/$CURRENT_BRANCH
-  mv ap-data-to-dashboard/node_modules/miravi/main/dist/* gh-pages/$CURRENT_BRANCH
+
+  if [[ "$USE_DIST" == "true" ]]; then
+    mv ap-data-to-dashboard/node_modules/miravi/main/dist/* gh-pages/$CURRENT_BRANCH
+  else
+    cp scripts/dashboard-placeholder.html gh-pages/$CURRENT_BRANCH/index.html
+  fi
 fi
