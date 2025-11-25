f4653cd0475eb57fd2536b7e7a1b5f67831ab624
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2025-11-25 11:52:31 +0100

    Remove directories before move/copy

--- scripts/move-miravi-dist.sh
+++ scripts/move-miravi-dist.sh
@@ -10,6 +10,7 @@ echo $CURRENT_BRANCH
 
 if [[ "$CURRENT_BRANCH" == "main" ]]; then
   echo "Not using subdirectory because branch is main."
+  rm -rf gh-pages/assets gh-pages/images gh-pages/queries
 
   if [[ "$USE_DIST" == "true" ]]; then
     mv ap-data-to-dashboard/node_modules/miravi/main/dist/* gh-pages
@@ -18,6 +19,7 @@ if [[ "$CURRENT_BRANCH" == "main" ]]; then
   fi
 else
   echo "Using subdirectory because branch is not main."
+  rm -rf gh-pages/$CURRENT_BRANCH
   mkdir -p gh-pages/$CURRENT_BRANCH
 
   if [[ "$USE_DIST" == "true" ]]; then
